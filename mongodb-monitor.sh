b3fd26fc5113575d7d7836acd4b6ffd8b178391b
Vinaum8 <vinaumpt@gmail.com>
2020-09-22 14:04:01 -0300

    add notification telegram

--- mongodb-monitor.sh
+++ mongodb-monitor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#healthcheck="echo 'db.runCommand("ping").ok' | mongo localhost:27017/test --quiet"
 healthcheck="$(echo 'db.runCommand("ping").ok' | mongo localhost:27017/test --quiet)"
+chat_id='{chat_ID}'
+api_token='{API_TOKEN}'
 
 if [[ "$healthcheck" != "1" ]]
 then
 	echo "Mongodb Service não esta rodando"
+	curl -d chat_id="$chat_id" -d text="Serviço do Mongodb caiu" https://api.telegram.org/bot$api_token/sendMessage
 	sudo service mongod start
 fi
